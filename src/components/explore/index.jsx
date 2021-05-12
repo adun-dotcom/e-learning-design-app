91c6f7e1d63f2c8fd4f585d4dc2ea713cb75178c
Adunola Johnson <aduntobi7@gmail.com>
2021-05-12 05:07:57 +0100

    deleted files

--- src/components/explore/index.jsx
+++ src/components/explore/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import MyNav from '../navbar/index'
 import MyFooter from '../Footer'
 import CoursePg from '../../pages/Course'
 
 function ExplorePg() {
   return (
     <div>
+      <MyNav/>
       <CoursePg />
+      <MyFooter/>
     </div>
   )
 }
